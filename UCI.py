1d2b83f8be6ad52f8dae1de1dd50e47a6103a611
Isael Perez <pereztrianaisael@gmail.com>
2024-03-08 00:14:08 -0500

    Complete la espera de un paciente a otro

--- UCI.py
+++ UCI.py
@@ -1,23 +1,44 @@
 from simpy import Environment
 from procesar_datos import *
+from datetime import timedelta
 
 class UCI():
-    def __init__(self, env:Environment) -> None:
+    def __init__(self, env:Environment, path:str) -> None:
         self.env = env
+        #se crea un evento para un nuevo paciente
+        self.nuevo_paciente = env.event()
+
+        self.env.process(self.entrada_paciente_uci(path))
+        self.env.process(self.entrada_paciente(path))
 
     def entrada_paciente(self, path:str):
 
+        #Se obtienen los datos del archivo de entrada y se agregan a la cola de pacientes
         fecha_ing = get_fecha_ingreso(path)
 
         while True:
-            print(f"El paciente llego al hospital a las {self.env.now}")
-            yield self.env.process(self.entrada_paciente_uci())
-            espera_ingreso = -(next(fecha_ing)-next(fecha_ing))
-            yield self.env.timeout(espera_ingreso)
+
+            print(f"El paciente llego al hospital a las {self.env.now}h")
+
+            #Se ejecuta el evento nuevo paciente
+            self.nuevo_paciente.succeed()
+            self.nuevo_paciente = self.env.event()
+            #Se calcula la espera y se espera hasta que entre el paciente a la UCI
+            fecha_siguiente, fecha = next(fecha_ing)
+            espera_ingreso = fecha_siguiente - fecha
+            yield self.env.timeout(espera_ingreso.days * 24)
 
     def entrada_paciente_uci(self, path:str):
 
         fecha_ing_uci = get_fecha_ing_uci(path)
+        fecha_ingreso = get_fecha_ingreso(path)
 
         while True:
-            print(f"El paciente llega a la uci a las {self.env.now}")
+
+            yield self.nuevo_paciente
+
+            espera_uci = next(fecha_ing_uci) - next(fecha_ingreso)
+            yield self.env.timeout(espera_uci.days * 24)
+
+            print(f"El paciente llega a la uci a las {self.env.now}h")
+
--- procesar_datos.py
+++ procesar_datos.py
@@ -1,15 +1,19 @@
 import pandas as pd
 
 def get_fecha_ingreso(path:str):
-    df = pd.read_csv(path, index_col=0, parse_dates=["fecha_ingreso", "fecha_egreso",
+    df = pd.read_csv(path, parse_dates=["fecha_ingreso", "fecha_egreso",
                                                       "fecha_ing_uci", "fecha_egr_uci"])
+    df.sort_values(["fecha_ingreso"])
     fecha_ingreso = list(df["fecha_ingreso"].dt.date)
-    for fecha in fecha_ingreso:
-        yield fecha
+    fecha = fecha_ingreso[0]
+    for fecha_siguiente in fecha_ingreso:
+        yield (fecha_siguiente, fecha)
+        fecha = fecha_siguiente
 
 def get_fecha_egreso(path:str):
     df = pd.read_csv(path, index_col=0, parse_dates=["fecha_ingreso", "fecha_egreso",
                                                       "fecha_ing_uci", "fecha_egr_uci"])
+    df.sort_values(["fecha_ingreso"])
     fecha_ingreso = list(df["fecha_egreso"].dt.date)
     for fecha in fecha_ingreso:
         yield fecha
@@ -17,6 +21,7 @@ def get_fecha_egreso(path:str):
 def get_fecha_ing_uci(path:str):
     df = pd.read_csv(path, index_col=0, parse_dates=["fecha_ingreso", "fecha_egreso",
                                                       "fecha_ing_uci", "fecha_egr_uci"])
+    df.sort_values(["fecha_ingreso"])
     fecha_ingreso = list(df["fecha_ing_uci"].dt.date)
     for fecha in fecha_ingreso:
         yield fecha
@@ -24,7 +29,14 @@ def get_fecha_ing_uci(path:str):
 def get_fecha_egr_uci(path:str):
     df = pd.read_csv(path, index_col=0, parse_dates=["fecha_ingreso", "fecha_egreso",
                                                       "fecha_ing_uci", "fecha_egr_uci"])
+    df.sort_values(["fecha_ingreso"])
     fecha_ingreso = list(df["fecha_egr_uci"].dt.date)
     for fecha in fecha_ingreso:
         yield fecha
 
+def get_tiempo_vam(path:str):
+    df = pd.read_csv(path, index_col=0)
+    df.sort_values(["fecha_ingreso"])
+    tiempo_vam = list(df["tiempo_vam"])
+    for horas in  tiempo_vam:
+        yield horas
