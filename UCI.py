ef16a7e0295d8409ff0988d721ae04ebce1697f5
Isael Perez <pereztrianaisael@gmail.com>
2024-03-11 00:33:23 -0400

    Terminado el modelo de simulacion

--- UCI.py
+++ UCI.py
@@ -1,6 +1,5 @@
 from simpy import Environment
 from procesar_datos import *
-from datetime import timedelta
 
 class UCI:
     def __init__(self, env:Environment, path:str) -> None:
@@ -8,51 +7,83 @@ class UCI:
         #se crea un evento para un nuevo paciente
         self.nuevo_paciente = env.event()
 
-        self.env.process(self.entrada_paciente_uci(path))
         self.env.process(self.entrada_paciente(path))
 
     def entrada_paciente(self, path:str):
 
         #Se obtienen los datos del archivo de entrada y se agregan a la cola de pacientes
         gen_fecha_ing = get_fecha_ingreso(path)
+        paciente = 0
 
         while True:
+            paciente += 1
+            print(f"El paciente {paciente} llego al hospital a las {self.env.now}h")
 
-            print(f"El paciente llego al hospital a las {self.env.now}h")
-
-            #Se ejecuta el evento nuevo paciente
-            self.nuevo_paciente.succeed()
-            self.nuevo_paciente = self.env.event()
             #Se calcula la espera y se espera hasta que llegue un nuevo paciente
-            fecha_siguiente, fecha = next(gen_fecha_ing)
-            espera_ingreso = fecha_siguiente - fecha
-            yield self.env.timeout(espera_ingreso.days * 24)
+            try:
+                fecha_siguiente, fecha = next(gen_fecha_ing)
+                espera_ingreso = fecha_siguiente - fecha
+                yield self.env.timeout(espera_ingreso.days * 24)
+            except StopIteration:
+                break
+            finally:
+                self.env.process(self.entrada_paciente_uci(path, paciente))
 
-    def entrada_paciente_uci(self, path: str):
+    def entrada_paciente_uci(self, path: str, paciente: int):
+
+        #Se inicializan los generadores necesarios
         gen_fecha_ing_uci = get_fecha_ing_uci(path)
         gen_fecha_ingreso = get_fecha_ingreso(path)
         gen_estadia_uci = get_estadia_uci(path)
         gen_tiempo_van = get_tiempo_vam(path)
-        paciente = 1
 
         while True:
 
-            yield self.nuevo_paciente
-
+            #Se calcula el tiempo de espera de entrada a la uci y se espera
             fecha_ing_uci = next(gen_fecha_ing_uci)
             fecha_ingreso = next(gen_fecha_ingreso)
-            espera_uci = fecha_ing_uci - fecha_ingreso
+            espera_uci = fecha_ing_uci - fecha_ingreso[1]
+
             yield self.env.timeout(espera_uci.days * 24)
 
             print(f"El paciente {paciente} llega a la uci a las {self.env.now}h")
             print(f"El paciente {paciente} se le pone ventilacion artificial a las {self.env.now}h")
+
+            #Se espera el tiempo que el paciente pasa en van
             tiempo_van = next(gen_tiempo_van)
 
-            yield tiempo_van
+            yield self.env.timeout(tiempo_van)
 
             print(f"Al paciente {paciente} se le quita la ventilacion a las {self.env.now}h")
+
+            #Se calcula la salida de la uci y se espera a que suceda
             estadia_uci = next(gen_estadia_uci)
             salida_uci = estadia_uci * 24 - tiempo_van
 
-            yield salida_uci
+            yield self.env.timeout(salida_uci)
 
+            #Se decide a que sala ira el paciente
+            gen_sala_egreso = get_sala_egreso(path)
+            sala_egreso = next(gen_sala_egreso)
+            print(f"El paciente {paciente} salio de la uci y fue trasladado hacia {sala_egreso}")
+
+            #Se espera el egreso del paciente
+            gen_fecha_egreso = get_fecha_egreso(path)
+            egreso = next(gen_fecha_egreso)
+
+            yield self.env.timeout(egreso.day * 24)
+
+            #Se termina la simulacion
+            gen_evolucion = get_evolucion(path)
+            evolucion = next(gen_evolucion)
+
+            if evolucion == "vivo":
+                print(f"El paciente {paciente} se mantiene vivo y fue dado de alta a las {self.env.now}")
+                break
+            else:
+                print(f"El paciente {paciente} fallece a las {self.env.now}")
+                break
+
+env = Environment()
+uci = UCI(env, "datos.csv")
+env.run()
--- procesar_datos.py
+++ procesar_datos.py
@@ -1,60 +1,61 @@
 import pandas as pd
 
-def get_fecha_ingreso(path: str):
+def cargar_fichero(path: str, column:str) -> list:
     df = pd.read_csv(
         path,
         index_col=0,
         parse_dates=["fecha_ingreso", "fecha_egreso", "fecha_ing_uci", "fecha_egr_uci"],
     )
+    df["tiempo_vam"] = df["tiempo_vam"].astype(int)
+    estadia = list(df.sort_values("fecha_ingreso")[column])
+    return estadia
+def get_fecha_ingreso(path: str):
 
-    fecha_ingreso = list(df.sort_values("fecha_ingreso")["fecha_ingreso"])
+    fecha_ingreso = cargar_fichero(path, "fecha_ingreso")
     fecha = fecha_ingreso[0]
+
     for fecha_siguiente in fecha_ingreso:
         yield (fecha_siguiente, fecha)
         fecha = fecha_siguiente
 
 def get_fecha_egreso(path: str):
-    df = pd.read_csv(
-        path,
-        index_col=0,
-        parse_dates=["fecha_ingreso", "fecha_egreso", "fecha_ing_uci", "fecha_egr_uci"],
-    )
-    fecha_ingreso = list(df.sort_values("fecha_ingreso")["fecha_egreso"].dt.date)
+
+    fecha_ingreso = cargar_fichero(path, "fecha_egreso")
     for fecha in fecha_ingreso:
         yield fecha
 
 def get_fecha_ing_uci(path: str):
-    df = pd.read_csv(
-        path,
-        index_col=0,
-        parse_dates=["fecha_ingreso", "fecha_egreso", "fecha_ing_uci", "fecha_egr_uci"],
-    )
-    fecha_ingreso = list(df.sort_values("fecha_ingreso")["fecha_ing_uci"].dt.date)
+
+    fecha_ingreso = cargar_fichero(path, "fecha_ing_uci")
     for fecha in fecha_ingreso:
         yield fecha
 
 def get_tiempo_vam(path:str):
-    df = pd.read_csv(path, index_col=0)
-    tiempo_vam = list(df.sort_values("fecha_ingreso")["tiempo_vam"])
+
+    tiempo_vam = cargar_fichero(path, "tiempo_vam")
     for horas in  tiempo_vam:
         yield horas
 
 def get_fecha_egr_uci(path: str):
-    df = pd.read_csv(
-        path,
-        index_col=0,
-        parse_dates=["fecha_ingreso", "fecha_egreso", "fecha_ing_uci", "fecha_egr_uci"],
-    )
-    fecha_ingreso = list(df.sort_values("fecha_ingreso")["fecha_egr_uci"].dt.date)
+
+    fecha_ingreso = cargar_fichero(path, "fecha_egr_uci")
     for fecha in fecha_ingreso:
         yield fecha
 
 def get_estadia_uci(path: str):
-    df = pd.read_csv(
-        path,
-        index_col=0,
-        parse_dates=["fecha_ingreso", "fecha_egreso", "fecha_ing_uci", "fecha_egr_uci"],
-    )
-    estadia = list(df.sort_values("fecha_ingreso")["estadia_uci"])
+
+    estadia = cargar_fichero(path, "estadia_uci")
     for est in estadia:
         yield est
+
+def get_sala_egreso(path: str):
+
+    estadia = cargar_fichero(path, "sala_egreso")
+    for est in estadia:
+        yield est
+
+def get_evolucion(path: str):
+
+    evoluciones = cargar_fichero(path, "evolucion")
+    for evolucion in evoluciones:
+        yield evolucion
