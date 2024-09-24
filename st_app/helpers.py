46ccf412909037f8398e839ff8cb7efe9e8764d8
coslatte <gabrielpazruiz02@gmail.com>
2024-09-24 13:38:48 -0400

    Add lógica de validación de campos en la aplicación. Cambios en organización de variables constantes y funciones helpers para la aplicación.

---
+++ st_app/helpers.py
@@ -0,0 +1,64 @@
+from soupsieve.util import lower
+
+from utils.constants.categories import TIPO_VENT, DIAG_PREUCI, INSUF_RESP
+
+
+def key_categ(categoria: str, valor: str | int, viceversa: bool = False) -> int | str:
+    """
+    Obtiene la llave (key, k) que constituye un valor si está presente en la colección de categorías definidas.
+
+    Args:
+        categoria: Las categorías deben ser entre "va", "diag" e "insuf".
+        valor: Es el valor que se pasa para buscar su llave.
+        viceversa: Determina si en lugar de buscar el valor, se busca la llave (key).
+
+    Returns:
+        Llave que representa en las colecciones de categorías el valor que se pasa por parámetros.
+    """
+
+    match categoria:
+        case "va":
+            categorias = TIPO_VENT
+        case "diag":
+            categorias = DIAG_PREUCI
+        case "insuf":
+            categorias = INSUF_RESP
+        case _:
+            raise Exception(f"La categoría que se selecciona no existe {categoria}.")
+    for k, v in categorias.items():
+        if not viceversa:
+            if v == valor:
+                return k
+        else:
+            if k == valor:
+                return v
+
+    if not viceversa:
+        raise Exception(f"El valor (value) que se proporcionó no se encuentra en el conjunto de categorías {categoria}")
+    else:
+        raise Exception(f"La llave (key) que se proporcionó no se encuentra en el conjunto de categórias {categoria}")
+
+
+def value_is_zero(valores: list[int | str] | int | str) -> bool:
+    """
+    Verifica si todos los valores son 0 o "Vacío".
+
+    Args:
+        valores: Valor o lista de valores a verificar.
+
+    Returns:
+        `true` si el valor o valores son 0 o "vacío", `false` caso contrario.
+    """
+    if isinstance(valores, int | str):
+        return __iszero(valores)
+    elif isinstance(valores, list):
+        return all(__iszero(v) for v in valores)
+    else:
+        raise ValueError(f"El valor a verificar no es correcto: {valores}")
+
+
+def __iszero(v: int | str) -> bool:
+    if isinstance(v, int):
+        return v == 0
+    elif isinstance(v, str):
+        return lower(v) == "vacío"
