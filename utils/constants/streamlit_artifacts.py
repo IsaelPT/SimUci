42b103004f1034e762d8134d594409ee335e05b2
coslatte <gabrielpazruiz02@gmail.com>
2024-09-24 13:39:49 -0400

    Remove streamlit_artifacts.py.

--- utils/constants/streamlit_artifacts.py
+++
@@ -1,5 +0,0 @@
-# Mensajes de Ayuda en varios Widgets de la aplicación Streamlit
-HELP_MSG_APACHE: str = "Valor del APACHE."
-HELP_MSG_ESTAD_UTI: str = "Tiempo de estadía en UTI."
-HELP_MSG_ESTAD_PREUTI: str = "Tiempo de estadía pre UTI"
-HELP_MSG_CORRIDA_SIM: str = "La cantidad de corridas de la simulación brinda un mayor margen de precisión."
