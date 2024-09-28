4567fba16853c11b9926549030a557fc923a81e6
Isael Perez <pereztrianaisael@gmail.com>
2024-09-28 10:48:06 -0400

    Added: Agregado los test estadisticos de Friedman y el intervalo de confianza

--- uci/stats.py
+++ uci/stats.py
@@ -1,4 +1,6 @@
-from scipy.stats import wilcoxon
+from scipy.stats import wilcoxon, friedmanchisquare
+from scipy import stats
+import numpy as np
 
 
 class Stats:
@@ -6,3 +8,13 @@ class Stats:
     def wilcoxon(x, y) -> tuple:
         res = wilcoxon(x=x, y=y)
         return res
+
+    @staticmethod
+    def friedman(*samples) -> tuple:
+        res = friedmanchisquare(samples)
+        return res
+
+    @staticmethod
+    def confidenceinterval(mean, std, n):
+        conf_int = stats.norm.interval(0.95, loc=mean, scale=std/np.sqrt(n))
+        return conf_int
