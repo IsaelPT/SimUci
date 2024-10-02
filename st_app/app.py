a8cb4ff7ab5c20136ebe78f55f85cc626bd25ef0
gpcosmic <129236813+coslatte@users.noreply.github.com>
2024-10-02 06:41:18 -0400

    Update lógica de Test Wilcoxon, Test Friedman.

--- st_app/app.py
+++ st_app/app.py
@@ -2,14 +2,44 @@ import os.path
 from datetime import datetime
 
 import streamlit as st
+from pandas.io.formats.style import Styler
 from scipy.stats import wilcoxon
 
 from constants import *
 from helpers import *
 from uci.experiment import *
 
+
+#### FUNCTIONS
+def build_data_to_show(style: bool = True, show_index: bool = True) -> DataFrame | Styler:
+    datos_paciente = {
+        "Edad": [opcion_edad, None],
+        "Diagnóstico 1": [opcion_diagn1, diagn1],
+        "Diagnóstico 2": [opcion_diagn2, diagn2],
+        "Diagnóstico 3": [opcion_diagn3, diagn3],
+        "Diagnóstico 4": [opcion_diagn4, diagn4],
+        "Apache": [opcion_apache, None],
+        "Tiempo Ventilación Artificial": [opcion_tiempo_vam, None],
+        "Tipo Ventilación Artificial": [opcion_tipo_vam, tipo_vam],
+        "Estadía UTI": [opcion_estad_uti, None],
+        "Estadía Pre-UTI": [opcion_estad_preuti, None],
+        "Insuficiencia Respiratoria": [opcion_insuf_resp, insuf_resp],
+    }
+    df = pd.DataFrame(datos_paciente)
+
+    # Options
+    if show_index:
+        index = ["Valor", "Índice"]
+        df.index = index
+    else:
+        df = df.drop(df.index[-1])  # No mostrar la última fila.
+    if style:
+        return df.style.format(precision=2)
+    return df
+
+
 #### TABS
-simulacion_tab, comparacion_paciente_tab = st.tabs(("Simulación", "Comparaciones Paciente"))
+simulacion_tab, comparaciones_tab, db_tab = st.tabs(("Simulación", "Comparaciones", "Datos"))
 with simulacion_tab:
     ############
     # Paciente #
@@ -20,13 +50,15 @@ with simulacion_tab:
     # WARNING: EL ID DEL PACIENTE ESTÁ ALMACENADO DENTRO DEL SESSION_STATE!
     if "id_paciente" not in st.session_state:
         st.session_state.id_paciente = generate_id()
-    col1_nuevo_paciente, col2_nuevo_paciente = st.columns([0.3, 1])
+    col1_nuevo_paciente, col2_nuevo_paciente, col3_nuevo_paciente = st.columns([1, 1, 1])
     with col1_nuevo_paciente:
         nuevo_paciente = st.button("Nuevo paciente")
         if nuevo_paciente:
             st.session_state.id_paciente = generate_id()
     with col2_nuevo_paciente:
         st.caption(f"ID Paciente: {st.session_state.id_paciente}")
+    with col3_nuevo_paciente:
+        usar_score: bool = st.toggle("Utilizar Score pronóstico")
 
     # Ingresar Datos Paciente
     paciente_column1, paciente_column2, paciente_column3 = st.columns(3)
@@ -39,7 +71,7 @@ with simulacion_tab:
         opcion_estad_uti: int = st.number_input("Estadía UTI", min_value=ESTAD_UTI_MIN, max_value=ESTAD_UTI_MAX,
                                                 value=ESTAD_UTI_DEFAULT, help=HELP_MSG_ESTAD_UTI)
     with paciente_column2:
-        opcion_diagn1: str = st.selectbox("Diagnostico 1", tuple(DIAG_PREUCI.values()), )
+        opcion_diagn1: str = st.selectbox("Diagnostico 1", tuple(DIAG_PREUCI.values()), index=1)
         opcion_diagn2: str = st.selectbox("Diagnostico 2", tuple(DIAG_PREUCI.values()), )
         opcion_tipo_vam: str = st.selectbox("Tipo de VA", tuple(TIPO_VENT.values()), )
         opcion_estad_preuti: int = st.number_input("Estadía Pre-UTI", min_value=ESTAD_PREUTI_MIN,
@@ -48,7 +80,7 @@ with simulacion_tab:
     with paciente_column3:
         opcion_diagn3: str = st.selectbox("Diagnostico 3", tuple(DIAG_PREUCI.values()), )
         opcion_diagn4: str = st.selectbox("Diagnostico 4", tuple(DIAG_PREUCI.values()), )
-        opcion_insuf_resp: str = st.selectbox("Tipo Insuficiencia Respiratoria", tuple(INSUF_RESP.values()))
+        opcion_insuf_resp: str = st.selectbox("Tipo Insuficiencia Respiratoria", tuple(INSUF_RESP.values()), index=1)
         porciento = st.number_input("Porciento", min_value=PORCIENTO_SIM_MIN, max_value=PORCIENTO_SIM_MAX,
                                     value=PORCIENTO_SIM_DEFAULT, help=HELP_MSG_PORCIENTO_SIM)
 
@@ -66,29 +98,13 @@ with simulacion_tab:
     insuf_resp: int = key_categ("insuf", opcion_insuf_resp)
 
     # Mostrar Datos Paciente
-    contenedor = st.container()
-    with contenedor:
-        col_check, col_toggle = st.columns(2)
-        with col_check:
-            mostrar_datos: bool = st.checkbox("Mostrar datos del paciente", value=False)
-        with col_toggle:
-            usar_score: bool = st.toggle("Utilizar Score pronóstico")
-        if mostrar_datos:
-            datos_paciente = {
-                "Edad": [opcion_edad, None],
-                "Diagnóstico 1": [opcion_diagn1, diagn1],
-                "Diagnóstico 2": [opcion_diagn2, diagn2],
-                "Diagnóstico 3": [opcion_diagn3, diagn3],
-                "Diagnóstico 4": [opcion_diagn4, diagn4],
-                "Apache": [opcion_apache, None],
-                "Tiempo Ventilación Artificial": [opcion_tiempo_vam, None],
-                "Tipo Ventilación Artificial": [opcion_tipo_vam, tipo_vam],
-                "Estadía UTI": [opcion_estad_uti, None],
-                "Estadía Pre-UTI": [opcion_estad_preuti, None],
-                "Insuficiencia Respiratoria": [opcion_insuf_resp, insuf_resp],
-            }
-            df = pd.DataFrame(datos_paciente, index=["Valor", "Índice"]).style.format(precision=0)
-            st.dataframe(df)
+    mostrar_datos: bool = st.expander("Mostrar datos del paciente", expanded=False)
+    with mostrar_datos:
+        toggle_precision = st.toggle("Datos precisos", value=False)
+        if toggle_precision:
+            st.dataframe(build_data_to_show(style=True))
+        else:
+            st.dataframe(build_data_to_show(style=False))
 
     ##############
     # Simulación #
@@ -133,10 +149,14 @@ with simulacion_tab:
         # Desarrollo de Simulación.
         if diag_ok and insuf_ok:
             try:
-                # Experimento.
-                experiment = Experiment(edad, diagn1, diagn2, diagn3, diagn4, apache, insuf_resp,
-                                        insuf_resp, estadia_uti, tiempo_vam, estadia_preuti, porciento)
-                resultado_experimento = multiple_replication(experiment, corridas_sim)
+                # experiment = Experiment(edad, diagn1, diagn2, diagn3, diagn4, apache, insuf_resp,
+                #                         insuf_resp, estadia_uti, tiempo_vam, estadia_preuti, porciento)
+                # resultado_experimento = multiple_replication(experiment, corridas_sim)
+
+                # Experimento / Simulación.
+                resultado_experimento = start_experiment(corridas_sim, edad, diagn1, diagn2, diagn3, diagn4, apache,
+                                                         insuf_resp, insuf_resp, estadia_uti, tiempo_vam,
+                                                         estadia_preuti, porciento)
 
                 # Guardar resultados.
                 path_base = f"experimentos\\paciente-id-{st.session_state.id_paciente}"
@@ -152,61 +172,146 @@ with simulacion_tab:
             except Exception as e:
                 st.exception(f"No se pudo efectuar la simulación.\n{e}")
 
-with comparacion_paciente_tab:
-    st.header("Comparaciones para un Paciente")
+with comparaciones_tab:
+    comparaciones_wilcoxon = st.expander("Comparación vía Wilcoxon", expanded=True)
+    comparaciones_friedman = st.expander("Comparación vía Friedman", expanded=True)
 
-    experimento1: UploadedFile
-    experimento2: UploadedFile
-    df_experimento1: DataFrame
-    df_experimento2: DataFrame
+    with comparaciones_wilcoxon:
+        st.header("Prueba de Wilcoxon")
 
-    # Subir datos con File Uploader.
-    col1_file_upl, col2_file_upl = st.columns(2)
-    with col1_file_upl:
-        experimento1 = st.file_uploader("Resultado Experimento 1")
-        if experimento1:
-            df_experimento1 = bin_to_df(experimento1)
-            if not df_experimento1.empty:
-                st.dataframe(df_experimento1, height=200)
-                st.write(f"Cantidad de Filas: {df_experimento1.shape[0]}")
-    with col2_file_upl:
-        experimento2 = st.file_uploader("Resultado Experimento 2")
-        if experimento2:
-            df_experimento2 = bin_to_df(experimento2)
-            if not df_experimento2.empty:
-                st.dataframe(df_experimento2, height=200)
-                st.write(f"Cantidad de Filas: {df_experimento2.shape[0]}")
 
-    # Selección de columna para comparación
-    seleccion_comparacion_cont = st.container()
-    with seleccion_comparacion_cont:
-        opcion_columna_comparacion = st.selectbox("Escoja una columna para comparación", VARIABLES_EXPERIMENTO)
+        # TEXTO = "La *prueba de Wilcoxon* se usa para comparar dos grupos de datos relacionados \
+        # y ver si hay diferencias significativas entre ellos. Útil cuando los datos no siguen \
+        # una distribución normal."
+        # st.markdown(TEXTO)
 
-    boton_comparacion = st.button("Realizar comparación", type="primary", use_container_width=True)
+        # Considerando que los datos reales forman parte de una base de datos.
+        # En la etapa actual, dicha base de datos está en formato .csv, que queda a despensas de un
+        # futuro a implementar como una base de datos SQL o No SQL; incierto ahora mismo.
 
-    # Comparación
-    if boton_comparacion:
-        if not df_experimento1.empty and not df_experimento2.empty:
-            x: DataFrame = df_experimento1[opcion_columna_comparacion]
-            y: DataFrame = df_experimento2[opcion_columna_comparacion]
-            if not x.equals(y):
-                # Verificar que ambos dataframes tengan la misma cantidad de filas para realizar Wilcoxon.
-                len_dif = abs(len(x) - len(y))
-                len_warning_msg = lambda \
-                        exp: f"Se eliminaron filas del experimento {exp} para coincidir \
-                        con el experimento {2 if exp == 1 else 1} ({len_dif} filas diferentes)."
-                if x.shape[0] > y.shape[0]:  # La cantidad de filas de x, excede las de y.
-                    x = x.head(y.shape[0])
-                    st.warning(len_warning_msg(1))
-                elif y.shape[0] > x.shape[0]:  # La cantidad de filas de y, excede las de x.
-                    y = y.head(x.shape[0])
-                    st.warning(len_warning_msg(2))
+        def init_real_data() -> pd.DataFrame:
+            # Brinda un DataFrame con datos reales de la base de datos y con un formato para mostrar.
+
+            real_data = pd.DataFrame(get_real_data(), index=VARIABLES_EXPERIMENTO, columns=["Valor"])
+            real_data.index.name = "Datos"
+            return real_data
+
+
+        r_data: pd.DataFrame = init_real_data()
+        CAPTION_MSG_REAL_DATA: str = "Datos reales obtenidos de la Base de Datos"
+
+        experimento: UploadedFile
+        df_experimento: DataFrame = pd.DataFrame()
+
+        toggle_if_with_simulation_data = st.toggle("Realizar con datos de simulación", value=True)
+        if toggle_if_with_simulation_data:
+            if not st.session_state.df_resultado.empty:
+                df_experimento = st.session_state.df_resultado
+                with st.container():
+                    col1_wilcoxon, col2_wilcoxon = st.columns(2)
+                    with col1_wilcoxon:
+                        st.caption(CAPTION_MSG_REAL_DATA)
+                        st.dataframe(r_data, use_container_width=True, height=200)
+                    with col2_wilcoxon:
+                        st.caption("Media de datos resultantes de la Simulación")
+                        df_promedio = pd.DataFrame(st.session_state.df_resultado.mean(), columns=["Valor"])
+                        df_promedio.index.name = "Datos"
+                        st.dataframe(df_promedio, use_container_width=True, height=200)
+            else:
+                st.error("No se ha realizado ninguna simulación hasta el momento. \
+                Diríjase al apartado \"Simulación\".")
+        else:
+            # Escoger un archivo donde estén datos de simulación de un paciente.
+            col1_r_data, col2_file_upl = st.columns(2)
+            with col1_r_data:
+                st.caption(CAPTION_MSG_REAL_DATA)
+                st.dataframe(r_data, use_container_width=True, height=200)
+            with col2_file_upl:
+                experimento = st.file_uploader("Datos de resultado de Simulación")
+                if experimento:
+                    df_experimento = bin_to_df(experimento)
+                    if not df_experimento.empty:
+                        st.dataframe(df_experimento, height=200)
+
+        # Selección de columna para comparación
+        opcion_columna_comparacion = st.selectbox("Escoja una columna para la comparación", VARIABLES_EXPERIMENTO,
+                                                  key=1)
+        boton_comparacion = st.button("Realizar prueba", type="primary", use_container_width=True, key=2)
+        if boton_comparacion:
+            if not df_experimento.empty:
+                x: DataFrame = r_data[opcion_columna_comparacion]
+                y: DataFrame = df_experimento[opcion_columna_comparacion]
                 wilcoxon_data = (x, y)
                 resultado = wilcoxon(x, y)
                 st.write(resultado)
             else:
-                st.error("Imposible realizar prueba de Wilcoxon cuando la diferencia entre los \
-                elementos de \"x\" y \"y\" es cero para todos los elementos.")
-        else:
-            st.error("No se puede realizar la comparación. \
-            Se detectan datos vacíos o incompletos con los experimentos.")
+                st.warning("No se han cargado datos del experimento aún.")
+
+    with comparaciones_friedman:
+        st.header("Prueba de Friedman")
+        # TEXTO = "La *prueba de Friedman* se usa para comparar dos o más grupos de datos relacionados. \
+        # Útil cuando los datos no siguen una distribución normal."
+        # st.markdown(TEXTO)
+
+        experimento1: UploadedFile
+        experimento2: UploadedFile
+        df_experimento1: DataFrame = pd.DataFrame()
+        df_experimento2: DataFrame = pd.DataFrame()
+
+        # Subir datos con File Uploader.
+        col1_file_upl, col2_file_upl = st.columns(2)
+        with col1_file_upl:
+            experimento1 = st.file_uploader("Resultado Experimento 1")
+            if experimento1:
+                df_experimento1 = bin_to_df(experimento1)
+                if not df_experimento1.empty:
+                    st.dataframe(df_experimento1, height=200)
+                    st.write(f"Cantidad de Filas: {df_experimento1.shape[0]}")
+                else:
+                    st.warning("No se han cargado datos del experimento 1 aún.")
+        with col2_file_upl:
+            experimento2 = st.file_uploader("Resultado Experimento 2")
+            if experimento2:
+                df_experimento2 = bin_to_df(experimento2)
+                if not df_experimento2.empty:
+                    st.dataframe(df_experimento2, height=200)
+                    st.write(f"Cantidad de Filas: {df_experimento2.shape[0]}")
+                else:
+                    st.warning("No se han cargado datos del experimento 2 aún.")
+
+        # Selección de columna para comparación
+        opcion_columna_comparacion = st.selectbox("Escoja una columna para la comparación", VARIABLES_EXPERIMENTO,
+                                                  key=3)
+        boton_comparacion = st.button("Realizar prueba", type="primary", use_container_width=True, key=4)
+
+        # Comparación
+        if boton_comparacion:
+            if not df_experimento1.empty and not df_experimento2.empty:
+                x: DataFrame = df_experimento1[opcion_columna_comparacion]
+                y: DataFrame = df_experimento2[opcion_columna_comparacion]
+                if not x.equals(y):
+                    # Verificar que ambos dataframes tengan la misma cantidad de filas para realizar Wilcoxon.
+                    len_dif = abs(len(x) - len(y))
+                    len_warning_msg = lambda \
+                            exp: f"Se eliminaron filas del experimento {exp} para coincidir \
+                            con el experimento {2 if exp == 1 else 1} ({len_dif} filas diferentes)."
+                    if x.shape[0] > y.shape[0]:  # La cantidad de filas de x, excede las de y.
+                        x = x.head(y.shape[0])
+                        st.warning(len_warning_msg(1))
+                    elif y.shape[0] > x.shape[0]:  # La cantidad de filas de y, excede las de x.
+                        y = y.head(x.shape[0])
+                        st.warning(len_warning_msg(2))
+                    wilcoxon_data = (x, y)
+                    resultado = wilcoxon(x, y)
+                    st.write(resultado)
+                else:
+                    st.error("Imposible realizar prueba de Wilcoxon cuando la diferencia entre los \
+                    elementos de \"x\" y \"y\" es cero para todos los elementos.")
+            else:
+                st.warning("No se puede realizar la comparación. \
+                Se detectan datos vacíos o falta de datos en los experimentos.")
+
+with db_tab:
+    st.markdown("Este es el conjunto de datos que se utilizan para realizar las pruebas de comparaciones.\
+                Son datos que han sido recopilados de pacientes reales en estudios anteriores realizados.")
+    st.dataframe(pd.read_csv(RUTA_CSV_DATOS2))
