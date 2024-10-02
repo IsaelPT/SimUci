cdf2e9c744b919f2a732c94b806a466bedc1f0b4
gpcosmic <129236813+coslatte@users.noreply.github.com>
2024-10-02 02:35:32 -0400

    Update lógica de funcionamiento de función de formato para visualizar DataFrames.

--- st_app/helpers.py
+++ st_app/helpers.py
@@ -84,37 +84,58 @@ def generate_id(n: int = 10) -> str:
         raise Exception(f"La cantidad de dígitos n={n} debe ser mayor distinta que 0.")
 
 
-def format_df(datos: DataFrame, format_time: bool = False) -> DataFrame:
+def format_df(datos: DataFrame, enhance_format: bool = False, data_at_beginning: bool = False) -> DataFrame:
     """
     Construye un nuevo DataFrame. Agrega al comienzo del dataframe el *promedio* y *desviación estándar* de todos los valores.
 
     Args:
+        data_at_beginning: Muestra los datos nuevos al principio del dataframe. En caso contrario, los muestra al final.
         datos: DataFrame base.
-        format_time: Si mostrar para cada número en la tabla el carácter "h" para expresar que los números están expresados en *horas*.
+        enhance_format: Usar solo si es para mostrar datos. Para cada número en la tabla el carácter "h" para expresar que los números están expresados en *horas*.
 
     Returns:
         DataFrame nuevo con nuevas filas de promedio y desviación estándar con los valores del DataFrame.
     """
 
     # Construir DataFrame (salida)
-    promedio = datos.mean()  # Promedio
-    desvest = datos.std()  # Desviación Estándar
-    nuevos_datos = pd.DataFrame([promedio, desvest], index=["Promedio", "Desviación Estándar"])
+    nuevos_datos = {
+        "Promedio": datos.mean(),
+        "Desviación Estándar": datos.std(),
+        "Intervalo Confianza": datos.std(),  # PROVISIONAL
+    }
+    n_datos_values = list(nuevos_datos.values())
+    n_datos_labels = list(nuevos_datos.keys())
 
-    label = "Información"
-    res = pd.concat([nuevos_datos, datos], axis=0).reset_index(drop=True)
-    res.insert(0, label, "")
-
-    res.loc[0, label] = "Promedio"
-    res.loc[1, label] = "Desviación Estándar"
+    df_nuevos_datos = pd.DataFrame(n_datos_values, index=n_datos_labels)
 
     len_datos = datos.shape[0]
-    len_nuevos_datos = nuevos_datos.shape[0]
-    for i in range(len_nuevos_datos, len_datos + len_nuevos_datos):
-        res.loc[i, label] = f"Iteración {i - len_nuevos_datos + 1}"
+    len_nuevos_datos = df_nuevos_datos.shape[0]
+
+    # Construir Labels y Columna Informativa.
+    LABEL_INF = "Información"
+
+    def build_labels_helper():
+        res.insert(0, LABEL_INF, "")
+        for index, label in enumerate(n_datos_labels):
+            print(index, ":", label)
+            if data_at_beginning:
+                res.loc[index, LABEL_INF] = label
+            else:
+                res.loc[len_datos + index, LABEL_INF] = label
+
+    if data_at_beginning:
+        res = pd.concat([df_nuevos_datos, datos], axis=0).reset_index(drop=True)
+        build_labels_helper()
+        for i in range(len_nuevos_datos, len_datos + len_nuevos_datos):
+            res.loc[i, LABEL_INF] = f"Iteración {i - len_nuevos_datos + 1}"
+    else:
+        res = pd.concat([datos, df_nuevos_datos], axis=0).reset_index(drop=True)
+        build_labels_helper()
+        for i in range(0, len_datos):
+            res.loc[i, LABEL_INF] = f"Iteración {i + 1}"
 
     # Formato
-    if format_time:
+    if enhance_format:
         def fmt(horas: int | float) -> str | int:
             if isinstance(horas, (int, float)):
                 return f"{horas / 24:.1f} días ({horas:.1f} h)"
