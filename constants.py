856c7cc75ea6dae2a33d6bbc7275e9163ba6f3ea
gpcosmic <129236813+coslatte@users.noreply.github.com>
2024-10-07 23:34:53 -0400

    Update constantes valores mínimos-máximos.

--- constants.py
+++ constants.py
@@ -1,38 +1,38 @@
-## Number Input
-# Edad
 import os.path
 
-EDAD_MIN = 13
+## Number Input
+# Edad
+EDAD_MIN = 14
 EDAD_MAX = 100
 EDAD_DEFAULT = EDAD_MIN
 
 # Apache
-APACHE_MIN = 1
-APACHE_MAX = 100
-APACHE_DEFAULT = 25
+APACHE_MIN = 0  # Excepcionalmente.
+APACHE_MAX = 36
+APACHE_DEFAULT = APACHE_MIN
 
 # Tiempo VAM
-T_VAM_MIN = 1
-T_VAM_MAX = 3600
-T_VAM_DEFAULT = 100
+T_VAM_MIN = 24
+T_VAM_MAX = 700
+T_VAM_DEFAULT = T_VAM_MIN
 
 # Estadía UTI
-ESTAD_UTI_MIN = 1
+ESTAD_UTI_MIN = 0
 ESTAD_UTI_MAX = 200
-ESTAD_UTI_DEFAULT = 25
+ESTAD_UTI_DEFAULT = ESTAD_UTI_MIN
 
-# Estadía Pre-UTI
-ESTAD_PREUTI_MIN = 1
-ESTAD_PREUTI_MAX = 200
-ESTAD_PREUTI_DEFAULT = 25
+# Estadía Pre-UTI -> días
+ESTAD_PREUTI_MIN = 0
+ESTAD_PREUTI_MAX = 34
+ESTAD_PREUTI_DEFAULT = ESTAD_PREUTI_MIN
 
 # Corridas Simulación
-CORRIDAS_SIM_MIN = 20
+CORRIDAS_SIM_MIN = 25
 CORRIDAS_SIM_MAX = 1000
 CORRIDAS_SIM_DEFAULT = CORRIDAS_SIM_MIN
 
 # Porciento
-PORCIENTO_SIM_MIN = 1
+PORCIENTO_SIM_MIN = 0
 PORCIENTO_SIM_MAX = 100
 PORCIENTO_SIM_DEFAULT = 10
 
@@ -41,7 +41,7 @@ HELP_MSG_APACHE: str = "Valor del APACHE."
 HELP_MSG_ESTAD_UTI: str = "Tiempo de estadía en UTI."
 HELP_MSG_ESTAD_PREUTI: str = "Tiempo de estadía pre UTI"
 HELP_MSG_CORRIDA_SIM: str = "La cantidad de corridas de la simulación brinda un mayor margen de precisión."
-HELP_MSG_PORCIENTO_SIM: str = "Índice de porciento para el experiemento."
+HELP_MSG_PORCIENTO_SIM: str = "Proporción de tiempo dentro de estancia UCI que se espera antes de estar en Ventilación."
 
 TIPO_VENT: dict[int, str] = {0: "Tubo endotraqueal", 1: "Traqueostomía", 2: "Ambas"}
 
