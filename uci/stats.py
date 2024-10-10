7563c19266cbddb9e9e6cca343fa6d86060ca701
gpcosmic <129236813+coslatte@users.noreply.github.com>
2024-10-09 21:38:29 -0400

    Reworked script Stats. Ahora incluye clases y campos como statistic y p_value en conjunto con sus tests.

--- uci/stats.py
+++ uci/stats.py
@@ -6,21 +6,32 @@ class Wilcoxon:
     def __init__(self, x, y):
         self.sample1 = x,
         self.sample2 = y
+        self.statistic: float = 0
+        self.p_value: float = 0
 
-    def wilcoxon_test(self) -> tuple:
+    def test(self) -> None:
         res = wilcoxon(self.sample1, self.sample2)
-        return res
+        self.statistic = res[0]
+        self.p_value = res[1]
 
 
 class Friedman:
     def __init__(self, *samples):
         self.samples = [*samples]
+        self.statistic: float = 0
+        self.p_value: float = 0
 
-    def friedman_test(self) -> tuple[float, float]:
+    def test(self) -> None:
         res = friedmanchisquare(self.samples)
-        return res
+        self.statistic = res[0]
+        self.p_value = res[1]
 
 
-def confidenceinterval(mean, std, n):
-    conf_int = norm.interval(0.95, loc=mean, scale=std / np.sqrt(n))
-    return conf_int
+class StatsUtils:
+    @staticmethod
+    def confidenceinterval(mean, std, n):
+        sem = std / np.sqrt(n)
+        conf_int = norm.interval(confidence=0.95, loc=mean, scale=sem)
+        arr_limite_inferior = conf_int[0]
+        arr_limite_superior = conf_int[1]
+        return arr_limite_inferior, arr_limite_superior
