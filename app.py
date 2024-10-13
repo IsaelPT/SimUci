f0b078cf287be6a7448ba6d1bfc80bdddde15872
gpcosmic <129236813+coslatte@users.noreply.github.com>
2024-10-13 15:03:05 -0400

    Reworked lógica de mostrar datos de simulación y mejora de código por parte de los widgets en el tab de paciente.

--- app.py
+++ app.py
@@ -1,14 +1,14 @@
 from datetime import datetime
 
+import streamlit as st
+
 from st_utils.constants import *
 from st_utils.helpers import *
 from uci.experiment import *
 from uci.stats import *
 
-# from scipy.stats import wilcoxon, friedmanchisquare
-
 #### TABS
-simulacion_tab, validacion_tab, comparaciones_tab = st.tabs(("Simulación", "Datos reales", "Comparaciones"))
+simulacion_tab, datos_reales_tab, comparaciones_tab = st.tabs(("Simulación", "Datos reales", "Comparaciones"))
 
 with simulacion_tab:
     ############
@@ -33,26 +33,28 @@ with simulacion_tab:
     # Ingresar Datos Paciente
     col1_paciente, col2_paciente, col3_paciente = st.columns(3)
     with col1_paciente:
-        opcion_edad: int = st.number_input("Edad", min_value=EDAD_MIN, max_value=EDAD_MAX, value=EDAD_DEFAULT)
-        opcion_apache: int = st.number_input("Apache", min_value=APACHE_MIN, max_value=APACHE_MAX, value=APACHE_DEFAULT,
-                                             help=HELP_MSG_APACHE)
-        opcion_tiempo_vam: int = st.number_input("Tiempo de Ventilación Artificial", min_value=T_VAM_MIN,
+        opcion_edad: int = st.number_input(label="Edad", min_value=EDAD_MIN, max_value=EDAD_MAX, value=EDAD_DEFAULT)
+        opcion_apache: int = st.number_input(label="Apache", min_value=APACHE_MIN, max_value=APACHE_MAX,
+                                             value=APACHE_DEFAULT, help=HELP_MSG_APACHE)
+        opcion_tiempo_vam: int = st.number_input(label="Tiempo de Ventilación Artificial", min_value=T_VAM_MIN,
                                                  max_value=T_VAM_MAX, value=T_VAM_DEFAULT)
-        opcion_estad_uti: int = st.number_input("Estadía UCI", min_value=ESTAD_UTI_MIN, max_value=ESTAD_UTI_MAX,
+        opcion_estad_uti: int = st.number_input(label="Estadía UCI", min_value=ESTAD_UTI_MIN, max_value=ESTAD_UTI_MAX,
                                                 value=ESTAD_UTI_DEFAULT, help=HELP_MSG_ESTAD_UTI)
     with col2_paciente:
-        opcion_insuf_resp: str = st.selectbox("Tipo Insuficiencia Respiratoria", tuple(INSUF_RESP.values()), index=1)
-        opcion_tipo_vam: str = st.selectbox("Tipo de Ventilación Artificial", tuple(TIPO_VENT.values()), )
-        opcion_estad_preuti: int = st.number_input("Estadía Pre-UCI", min_value=ESTAD_PREUTI_MIN,
+        opcion_insuf_resp: str = st.selectbox(label="Tipo Insuficiencia Respiratoria",
+                                              options=tuple(INSUF_RESP.values()), index=1)
+        opcion_tipo_vam: str = st.selectbox(label="Tipo de Ventilación Artificial", options=tuple(TIPO_VENT.values()), )
+        opcion_estad_preuti: int = st.number_input(label="Estadía Pre-UCI", min_value=ESTAD_PREUTI_MIN,
                                                    max_value=ESTAD_PREUTI_MAX, value=ESTAD_PREUTI_DEFAULT,
                                                    help=HELP_MSG_ESTAD_PREUTI)
-        porciento = st.number_input("Porciento Tiempo UCI", min_value=PORCIENTO_SIM_MIN, max_value=PORCIENTO_SIM_MAX,
-                                    value=PORCIENTO_SIM_DEFAULT, help=HELP_MSG_PORCIENTO_SIM)
+        input_porciento = st.number_input(label="Porciento Tiempo UCI", min_value=PORCIENTO_SIM_MIN,
+                                          max_value=PORCIENTO_SIM_MAX, value=PORCIENTO_SIM_DEFAULT,
+                                          help=HELP_MSG_PORCIENTO_SIM)
     with col3_paciente:
-        opcion_diagn1: str = st.selectbox("Diagnóstico 1", tuple(DIAG_PREUCI.values()), index=0)
-        opcion_diagn2: str = st.selectbox("Diagnóstico 2", tuple(DIAG_PREUCI.values()), index=0)
-        opcion_diagn3: str = st.selectbox("Diagnóstico 3", tuple(DIAG_PREUCI.values()), index=0)
-        opcion_diagn4: str = st.selectbox("Diagnóstico 4", tuple(DIAG_PREUCI.values()), index=0)
+        opcion_diagn1: str = st.selectbox(label="Diagnóstico 1", options=tuple(DIAG_PREUCI.values()), index=0)
+        opcion_diagn2: str = st.selectbox(label="Diagnóstico 2", options=tuple(DIAG_PREUCI.values()), index=0)
+        opcion_diagn3: str = st.selectbox(label="Diagnóstico 3", options=tuple(DIAG_PREUCI.values()), index=0)
+        opcion_diagn4: str = st.selectbox(label="Diagnóstico 4", options=tuple(DIAG_PREUCI.values()), index=0)
 
         # Datos Paciente Recolectados (Son los datos de entrada para ser procesados).
         edad: int = opcion_edad
@@ -85,14 +87,19 @@ with simulacion_tab:
                                        help=HELP_MSG_CORRIDA_SIM)
         boton_comenzar = st.button("Comenzar Simulación", type="primary", use_container_width=True)
 
-    # Mostrar DataFrame con resultado de la simulación para este paciente.
+    # Mostrar DataFrame con resultado de la simulación para ese paciente.
     if not st.session_state.df_resultado.empty:
-        toggle_fmt = st.toggle("Tabla con formato", value=True)
+        toggle_format = st.toggle("Formato de tiempo", value=True)
+        if toggle_format:
+            df_simulacion = format_df_time(build_df_stats(df=st.session_state.df_resultado,
+                                                          sample_size=corridas_sim, include_info_label=True))
+        else:
+            df_simulacion = build_df_stats(df=st.session_state.df_resultado, sample_size=corridas_sim,
+                                           include_info_label=True)
         st.dataframe(
-            format_df_time(st.session_state.df_resultado, toggle_fmt, data_at_beginning=True),
+            df_simulacion,
             hide_index=True,
             use_container_width=True,
-            height=300
         )
 
         # Lógica para guardar resultados localmente.
@@ -124,7 +131,7 @@ with simulacion_tab:
                 # Experimento / Simulación.
                 resultado_experimento = start_experiment(corridas_sim, edad, diagn1, diagn2, diagn3, diagn4, apache,
                                                          insuf_resp, insuf_resp, estadia_uti, tiempo_vam,
-                                                         estadia_preuti, porciento)
+                                                         estadia_preuti, input_porciento)
 
                 # Guardar resultados.
                 path_base = f"experimentos\\paciente-id-{st.session_state.id_paciente}"
@@ -139,53 +146,43 @@ with simulacion_tab:
             except Exception as e:
                 st.exception(f"No se pudo efectuar la simulación. Error asociado:\n{e}")
 
-with validacion_tab:
-    df_data = pd.read_csv(RUTA_FICHERODEDATOS_CSV)
+with datos_reales_tab:
+    ##############
+    # Validación #
+    ##############
+    st.header("Validación con Datos Reales")
 
-    if "table_selections" not in st.session_state:
-        st.session_state.table_selections = 0
+    df_data = pd.read_csv(RUTA_FICHERODEDATOS_CSV)
 
     st.markdown("Este es el conjunto de datos que se utilizan para realizar las pruebas de comparaciones.\
             Son datos que han sido recopilados de pacientes reales en estudios anteriores realizados.")
-    st.write()
 
-    df_selection: list[int] = st.dataframe(
+    df_selection: int = st.dataframe(
         df_data,
         key="data",
         on_select="rerun",
-        selection_mode=["multi-row"],
+        selection_mode=["single-row"],
         hide_index=True,
         height=300
     )["selection"]["rows"]  # dict -> {"selection": {"rows": [0, 1, 2, ...], "columns": []}}
-    st.session_state.table_selections = len(df_selection)
-    if st.session_state.table_selections <= 1:
-        st.info("Seleccione dos o más filas en la tabla de datos para calcular indicadores estadísticos.")
 
     # DataFrame Datos Reales
-    if st.session_state.table_selections >= 2:
-        df_real_data: DataFrame = get_real_data(RUTA_FICHERODEDATOS_CSV, row_selection=df_selection)
-        with st.container():
-            st.markdown("Indicadores estadísticos a partir de Datos reales seleccionados")
-            res = build_df_stats(df_real_data)
-            st.dataframe(
-                res,
-                hide_index=True,
-                use_container_width=True,
-            )
-    # DataFrame Simulacion
-    if not st.session_state.df_resultado.empty:  # Hacemos referencias a los resultados de la Simulación
-        df_experimento_sim = st.session_state.df_resultado
-        sample_size = df_experimento_sim.shape[0]
-        with st.container():
-            st.markdown("Indicadores estadísticos a partir de Datos de la Simulación")
-            res = build_df_stats(df_experimento_sim)
-            st.dataframe(
-                res,
-                hide_index=True,
-                use_container_width=True,
-            )
-    else:
-        st.error("No se ha realizado ninguna simulación hasta el momento. Diríjase al apartado \"Simulación\".")
+    if df_selection:
+        st.markdown("Indicadores estadísticos a partir de Paciente seleccionado")
+        t: tuple[float] = extract_real_data(RUTA_FICHERODEDATOS_CSV, index=df_selection, return_type="tuple")
+        e = start_experiment(
+            corridas_simulacion=corridas_sim,
+            edad=int(t[0]), d1=t[1], d2=t[2], d3=t[3], d4=t[4],
+            apache=t[5], insuf_resp=t[6], va=t[7], est_uci=t[8], t_vam=t[9], est_preuti=t[10],
+            porciento=input_porciento
+        )
+        # DataFrame con experimento con datos reales
+        df_sim_datos_reales = build_df_stats(e, corridas_sim, include_info_label=True)
+        st.dataframe(
+            df_sim_datos_reales,
+            hide_index=True,
+            use_container_width=True,
+        )
 
 with comparaciones_tab:
     comparacion_wilcoxon = st.expander("Comparación vía Wilcoxon", expanded=True)
@@ -256,9 +253,7 @@ with comparaciones_tab:
                             # Mostrar Resultado
                             df_mostrar = build_df_test_result(statistic=wilcoxon_data.statistic,
                                                               p_value=wilcoxon_data.p_value)
-                            st.dataframe(df_mostrar,
-                                         # column_config=colm_template(["Statistics", "Valor de P"]),
-                                         hide_index=True, use_container_width=True)
+                            st.dataframe(df_mostrar, hide_index=True, use_container_width=True)
                         except Exception as e:
                             st.exception(e)
 
